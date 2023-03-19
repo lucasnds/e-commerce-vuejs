fb465dc984b091002c68649fbe736efb47517d48
Lucas <lucasdoismile16@gmail.com>
2023-03-19 17:34:49 -0300

    teste desfeito

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div id="nav">
-      <router-link to="/"> <fa icon="home"/> Teste</router-link> 
+      <router-link to="/"> <fa icon="home"/> Início</router-link> 
       <div>
         <router-link to="/accessories"> Acessórios </router-link> -
         <router-link to="/electronics"> Eletrônicos </router-link> -
